de35ac0041d1f20a239f9e865aa79ac038c2b7c1
Victor Rojas <vjrojasb@gmail.com>
2015-05-25 16:36:03 -0500

    ADD: clases para modelar el calculo de probabilidades

--- src/TA03/Variable.java
+++ src/TA03/Variable.java
@@ -5,12 +5,43 @@
  */
 package TA03;
 
+import java.util.List;
+
 /**
  *
  * @author Vj
  */
 public class Variable {
-    private int nombre;
-    private int contador;
+    private String nombre;
+    private List<Valor> listaValores;
+    
+    public double calculaProbabilidadMarginal(int numIncidencias, int total){
+        return  numIncidencias/total;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    
+    public List<Valor> getValores() {
+        return listaValores;
+    }
+
+    public void setValores(List<Valor> valores) {
+        this.listaValores = valores;
+    }
+
+    public void agregarValor(Valor v){
+        listaValores.add(v);
+    }
+    
+    public Valor getValor(int indice){
+        return listaValores.get(indice);
+    }
+    
     
 }
